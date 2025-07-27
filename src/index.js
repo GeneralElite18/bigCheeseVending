162d95fdca043749af5432dadc45899fd6195ad1
Alex Gonzales <gonzales.alexzander18@gmail.com>
2025-07-27 16:40:55 -0600

    updated paths

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter hashType="hashbang" basename="/bigCheeseVending">
+  <HashRouter hashType="noslash">
     <App />
   </HashRouter>
 );
